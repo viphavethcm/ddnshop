26bf2b3652144f22f1bfcffa60d31c91e4bcce11
viphavethcm <loivang.nhatduy@gmail.com>
2020-09-14 15:40:01 +0700

    config

---
+++ src/main/java/comddnshop/ddnshopmicroservices/DdnshopMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package comddnshop.ddnshopmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdnshopMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DdnshopMicroservicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/comddnshop/ddnshopmicroservices/DdnshopMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package comddnshop.ddnshopmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdnshopMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
